10f0e65e6c9f3a496ae869d677b078bd17745b62
berlina <berlina.lili@gmail.com>
2016-12-09 11:34:22 +0800

    update test.js

--- test.js
+++ test.js
@@ -5,11 +5,24 @@ document.getElementById("test").addEventListener('click', () => {
         //You can play with your DOM here or check URL against your regex
 
           console.log($('input#q'))
-          // $('input#q').value = '你好'
           $('input#q').val('Hello Kitty')
+          
+          // setTimeout(function(){
+          //    $('form[action*="/search"] [type=submit]').click()
+          // },2000)
+
+          $('form[action*="/search"] [type=submit]').click()
+
+          
+
+          //scoll to the product position
           setTimeout(function(){
-            $('button.btn-search.seahd-iconfont').click()
-          },2000)
+            $('html,body').animate({ scrollTop: 500 }, 'slow')
+            console.log("scoll to the product position")
+          },5000)
+       
+         
+   
     }
 
     //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
@@ -21,3 +34,51 @@ document.getElementById("test").addEventListener('click', () => {
         console.log(results[0]);
     });
 });
+
+
+
+
+
+// document.getElementById("test").addEventListener('click', () => {
+//   console.log("Popup DOM fully loaded and parsed");
+
+//   function modifyDOM() {
+//     var jQuery = $;
+    
+//     //type key word
+//     console.log("type key word")
+//     $('input#q').val('Hello Kitty')
+    
+//     //search product
+//     console.log("search product")
+//     setTimeout(function(){
+//       $('form[action*="/search"] [type=submit]').click()
+//     },2000)
+ 
+
+//     // //click the first product
+//     // console.log("click the first product")
+//     // setTimeout(function(){
+//     //   //var itemURL = 
+//     //   window.open($('.item').find('.title a').attr('href'),"_self");
+//     // },10000)
+
+
+
+//     //go back to the search page
+//     // console.log("go back to the main page")
+//     // setTimeout(function(){
+//     //   window.back();
+//     // },10000)
+
+//   }    
+
+//     //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+//   chrome.tabs.executeScript({
+//     code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+//   }, (results) => {
+//         //Here we have just the innerHTML and not DOM structure
+//         console.log('Popup script:')
+//         console.log(results[0]);
+//   });
+// });
