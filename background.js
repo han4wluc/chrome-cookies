44675399ea3349edf9acc5240b7e657de4885126
berlina <berlina.lili@gmail.com>
2016-12-13 14:08:10 +0800

    update background.js (first 3 steps)

--- background.js
+++ background.js
@@ -1,18 +1,95 @@
-
 $( document ).ready(function() {
-  var lastname = localStorage.getItem("keyword");
-  console.log('ready', lastname);
-  if(!lastname){
-    return;
+  // var lastname = localStorage.getItem("keyword");
+  // console.log('ready', lastname);
+  // if(!lastname){
+  //   return;
+  // }
+
+  // localStorage.removeItem("keyword");
+  // if(!window.doStartAutomation){
+  //   return;
+  // }
+
+  //step 3 taobao
+  if(window.doStartStep3 == 'yes'){
+
+    setTimeout(function(){
+      var scrollSpeed = $(document).height()/2; 
+      if($('.related-items').length){     
+          jQuery('html,body').animate({
+          scrollTop: jQuery('.related-items').offset().top-100}, scrollSpeed);
+      }else{
+        $('html, body').animate({
+          scrollTop: $(document).height()
+        }, scrollSpeed);
+      }
+      // window.close();
+    },5000)
+        
+    return
   }
 
-  localStorage.removeItem("keyword");
+  //step 3 tmall
+  if(window.location.href.indexOf("https://detail.tmall.com") >= 0){
+    console.log("tmall");
+    window.close();
+    return
+  }
 
-  setTimeout(function(){
-    $('input#q').val('Hello Kitty')
+
+    //step 2
+  console.log("startstep2", sessionStorage.getItem('doStartStep2'))
+  if(sessionStorage.getItem('doStartStep2') == "yes"){
+
+    sessionStorage.removeItem('doStartStep2')
+    $('html,body').animate({ scrollTop: 500 }, 'slow');
+
     setTimeout(function(){
-      $('button.btn-search.seahd-iconfont').click()
+      var link = $('.item').find('.title a')[5].href;
+      var w1 = window.open(link);
+      w1.doStartStep3='yes';
     },2000)
-  },2000)
+  }
 
+
+ 
+  // w1.doStartAutomation=true;  
+
+  // var scrollSpeed = $(document).height()/2; 
+  // if($('.related-items').length){     
+  //     jQuery('html,body').animate({
+  //     scrollTop: jQuery('.related-items').offset().top-100}, scrollSpeed);
+  // }else{
+  //   $('html, body').animate({
+  //     scrollTop: $(document).height()
+  //   }, scrollSpeed);
+  // }
+      
+  // window.close(link);
+ 
 });
+
+
+
+  // if(window.location.href.indexOf("https://detail.tmall.com") >= 0){
+  //   console.log("tmall");
+  //   window.close();
+  // }
+  // if(sessionStorage.getItem("doStartAutomation") == "yes"){
+  //   console.log("start automation");
+  //   setTimeout(function(){
+  //     console.log("success")
+  //   }, 5000)
+  // }   
+
+
+ 
+
+ //  var pathname = window.location.pathname
+ //  console.log(pathname)
+
+ //  if(pathname.indexOf("&close=true") >= 0){
+
+ //  }
+
+
